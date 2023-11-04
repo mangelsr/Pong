b70ed32257b58e43af246f1b6c7b7bb056789867
Miguel Sánchez <mangelsr25@gmail.com>
2023-11-03 21:31:25 -0500

    [INIT]

---
+++ Ball/ball.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+var speed = 500
+
+func _ready():
+	set_ball_velocity()
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+
+func set_ball_velocity():
+	if randi() % 2 == 0:
+		velocity.x = 1
+	else:
+		velocity.x = -1
+
+	if randi() % 2 == 0:
+		velocity.y = 1
+	else:
+		velocity.y = -1
+
+	velocity *= speed
---
+++ Pong/pong.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+const CENTER = Vector2(640, 360)
+var player_score = 0
+var computer_score = 0
+
+func _on_goal_left_body_entered(body):
+	computer_score += 1
+	$ComputerScore.text = str(computer_score)
+	reset()
+	
+func _on_goal_rigth_body_entered(body):
+	player_score += 1
+	$PlayerScore.text = str(player_score)
+	reset()
+
+func reset():
+	$Ball.position = CENTER
+	$Ball.call("set_ball_velocity")
+	$Player.position.y = CENTER.y
+	$Computer.position.y = CENTER.y
